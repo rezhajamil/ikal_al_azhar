0d33365e679fe0f6a9987fdebfdb753ba58ed762
John Doe <yamatogija@gmail.com>
2023-07-20 15:16:21 +0700

    Fix Struktur

--- resources/views/section/struktur.blade.php
+++ resources/views/section/struktur.blade.php
@@ -18,7 +18,148 @@
             <div class="w-full px-4 md:w-1/2 xl:w-1/4 h-[450px]">
                 <div class="mx-auto mb-10 w-full max-w-[370px]">
                     <div class="relative overflow-hidden rounded-lg">
-                        <img src="{{ asset('images/pembina.jpeg') }}" alt="image" class="w-full h-[450px]" />
+                        <img src="{{ asset('images/pembina1.jpg') }}" alt="image"
+                            class="w-full h-[450px] object-cover" />
+                        <div class="absolute left-0 w-full text-center bottom-5">
+                            <div class="relative px-3 py-5 mx-5 overflow-hidden bg-white rounded-lg">
+                                <h3 class="text-sm font-semibold text-dark">
+                                    Dr. Zulfahmi Lubis, Lc., MA.
+                                </h3>
+                                <p class="text-sm text-body-color">Pembina</p>
+                                <div>
+                                    <span class="absolute bottom-0 left-0">
+                                        <svg width="61" height="30" viewBox="0 0 61 30" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <circle cx="16" cy="45" r="45" fill="#13C296"
+                                                fill-opacity="0.3" />
+                                        </svg>
+                                    </span>
+                                    <span class="absolute top-0 right-0">
+                                        <svg width="20" height="25" viewBox="0 0 20 25" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <circle cx="0.706257" cy="24.3533" r="0.646687"
+                                                transform="rotate(-90 0.706257 24.3533)" fill="#13C296" />
+                                            <circle cx="6.39669" cy="24.3533" r="0.646687"
+                                                transform="rotate(-90 6.39669 24.3533)" fill="#13C296" />
+                                            <circle cx="12.0881" cy="24.3533" r="0.646687"
+                                                transform="rotate(-90 12.0881 24.3533)" fill="#13C296" />
+                                            <circle cx="17.7785" cy="24.3533" r="0.646687"
+                                                transform="rotate(-90 17.7785 24.3533)" fill="#13C296" />
+                                            <circle cx="0.706257" cy="18.6624" r="0.646687"
+                                                transform="rotate(-90 0.706257 18.6624)" fill="#13C296" />
+                                            <circle cx="6.39669" cy="18.6624" r="0.646687"
+                                                transform="rotate(-90 6.39669 18.6624)" fill="#13C296" />
+                                            <circle cx="12.0881" cy="18.6624" r="0.646687"
+                                                transform="rotate(-90 12.0881 18.6624)" fill="#13C296" />
+                                            <circle cx="17.7785" cy="18.6624" r="0.646687"
+                                                transform="rotate(-90 17.7785 18.6624)" fill="#13C296" />
+                                            <circle cx="0.706257" cy="12.9717" r="0.646687"
+                                                transform="rotate(-90 0.706257 12.9717)" fill="#13C296" />
+                                            <circle cx="6.39669" cy="12.9717" r="0.646687"
+                                                transform="rotate(-90 6.39669 12.9717)" fill="#13C296" />
+                                            <circle cx="12.0881" cy="12.9717" r="0.646687"
+                                                transform="rotate(-90 12.0881 12.9717)" fill="#13C296" />
+                                            <circle cx="17.7785" cy="12.9717" r="0.646687"
+                                                transform="rotate(-90 17.7785 12.9717)" fill="#13C296" />
+                                            <circle cx="0.706257" cy="7.28077" r="0.646687"
+                                                transform="rotate(-90 0.706257 7.28077)" fill="#13C296" />
+                                            <circle cx="6.39669" cy="7.28077" r="0.646687"
+                                                transform="rotate(-90 6.39669 7.28077)" fill="#13C296" />
+                                            <circle cx="12.0881" cy="7.28077" r="0.646687"
+                                                transform="rotate(-90 12.0881 7.28077)" fill="#13C296" />
+                                            <circle cx="17.7785" cy="7.28077" r="0.646687"
+                                                transform="rotate(-90 17.7785 7.28077)" fill="#13C296" />
+                                            <circle cx="0.706257" cy="1.58989" r="0.646687"
+                                                transform="rotate(-90 0.706257 1.58989)" fill="#13C296" />
+                                            <circle cx="6.39669" cy="1.58989" r="0.646687"
+                                                transform="rotate(-90 6.39669 1.58989)" fill="#13C296" />
+                                            <circle cx="12.0881" cy="1.58989" r="0.646687"
+                                                transform="rotate(-90 12.0881 1.58989)" fill="#13C296" />
+                                            <circle cx="17.7785" cy="1.58989" r="0.646687"
+                                                transform="rotate(-90 17.7785 1.58989)" fill="#13C296" />
+                                        </svg>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full px-4 md:w-1/2 xl:w-1/4">
+                <div class="mx-auto mb-10 w-full max-w-[370px]">
+                    <div class="relative overflow-hidden rounded-lg">
+                        <img src="{{ asset('images/pembina2.jpg') }}" alt="image"
+                            class="w-full h-[450px] object-cover" />
+                        <div class="absolute left-0 w-full text-center bottom-5">
+                            <div class="relative px-3 py-5 mx-5 overflow-hidden bg-white rounded-lg">
+                                <h3 class="text-sm font-semibold text-dark">
+                                    H. Fahruddin Lubis, Lc.
+                                </h3>
+                                <p class="text-sm text-body-color">Pembina</p>
+                                <div>
+                                    <span class="absolute bottom-0 left-0">
+                                        <svg width="61" height="30" viewBox="0 0 61 30" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <circle cx="16" cy="45" r="45" fill="#13C296"
+                                                fill-opacity="0.3" />
+                                        </svg>
+                                    </span>
+                                    <span class="absolute top-0 right-0">
+                                        <svg width="20" height="25" viewBox="0 0 20 25" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <circle cx="0.706257" cy="24.3533" r="0.646687"
+                                                transform="rotate(-90 0.706257 24.3533)" fill="#13C296" />
+                                            <circle cx="6.39669" cy="24.3533" r="0.646687"
+                                                transform="rotate(-90 6.39669 24.3533)" fill="#13C296" />
+                                            <circle cx="12.0881" cy="24.3533" r="0.646687"
+                                                transform="rotate(-90 12.0881 24.3533)" fill="#13C296" />
+                                            <circle cx="17.7785" cy="24.3533" r="0.646687"
+                                                transform="rotate(-90 17.7785 24.3533)" fill="#13C296" />
+                                            <circle cx="0.706257" cy="18.6624" r="0.646687"
+                                                transform="rotate(-90 0.706257 18.6624)" fill="#13C296" />
+                                            <circle cx="6.39669" cy="18.6624" r="0.646687"
+                                                transform="rotate(-90 6.39669 18.6624)" fill="#13C296" />
+                                            <circle cx="12.0881" cy="18.6624" r="0.646687"
+                                                transform="rotate(-90 12.0881 18.6624)" fill="#13C296" />
+                                            <circle cx="17.7785" cy="18.6624" r="0.646687"
+                                                transform="rotate(-90 17.7785 18.6624)" fill="#13C296" />
+                                            <circle cx="0.706257" cy="12.9717" r="0.646687"
+                                                transform="rotate(-90 0.706257 12.9717)" fill="#13C296" />
+                                            <circle cx="6.39669" cy="12.9717" r="0.646687"
+                                                transform="rotate(-90 6.39669 12.9717)" fill="#13C296" />
+                                            <circle cx="12.0881" cy="12.9717" r="0.646687"
+                                                transform="rotate(-90 12.0881 12.9717)" fill="#13C296" />
+                                            <circle cx="17.7785" cy="12.9717" r="0.646687"
+                                                transform="rotate(-90 17.7785 12.9717)" fill="#13C296" />
+                                            <circle cx="0.706257" cy="7.28077" r="0.646687"
+                                                transform="rotate(-90 0.706257 7.28077)" fill="#13C296" />
+                                            <circle cx="6.39669" cy="7.28077" r="0.646687"
+                                                transform="rotate(-90 6.39669 7.28077)" fill="#13C296" />
+                                            <circle cx="12.0881" cy="7.28077" r="0.646687"
+                                                transform="rotate(-90 12.0881 7.28077)" fill="#13C296" />
+                                            <circle cx="17.7785" cy="7.28077" r="0.646687"
+                                                transform="rotate(-90 17.7785 7.28077)" fill="#13C296" />
+                                            <circle cx="0.706257" cy="1.58989" r="0.646687"
+                                                transform="rotate(-90 0.706257 1.58989)" fill="#13C296" />
+                                            <circle cx="6.39669" cy="1.58989" r="0.646687"
+                                                transform="rotate(-90 6.39669 1.58989)" fill="#13C296" />
+                                            <circle cx="12.0881" cy="1.58989" r="0.646687"
+                                                transform="rotate(-90 12.0881 1.58989)" fill="#13C296" />
+                                            <circle cx="17.7785" cy="1.58989" r="0.646687"
+                                                transform="rotate(-90 17.7785 1.58989)" fill="#13C296" />
+                                        </svg>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full px-4 md:w-1/2 xl:w-1/4 h-[450px]">
+                <div class="mx-auto mb-10 w-full max-w-[370px]">
+                    <div class="relative overflow-hidden rounded-lg">
+                        <img src="{{ asset('images/pembina.jpeg') }}" alt="image"
+                            class="w-full h-[450px] object-cover" />
                         <div class="absolute left-0 w-full text-center bottom-5">
                             <div class="relative px-3 py-5 mx-5 overflow-hidden bg-white rounded-lg">
                                 <h3 class="text-sm font-semibold text-dark">
@@ -84,10 +225,13 @@
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="flex flex-wrap justify-center gap-4 -mx-4">
             <div class="w-full px-4 md:w-1/2 xl:w-1/4 h-[450px]">
                 <div class="mx-auto mb-10 w-full max-w-[370px]">
                     <div class="relative overflow-hidden rounded-lg">
-                        <img src="{{ asset('images/ketua.jpeg') }}" alt="image" class="w-full h-[450px]" />
+                        <img src="{{ asset('images/ketua.jpeg') }}" alt="image"
+                            class="w-full object-cover h-[450px]" />
                         <div class="absolute left-0 w-full text-center bottom-5">
                             <div class="relative px-3 py-5 mx-5 overflow-hidden bg-white rounded-lg">
                                 <h3 class="text-sm font-semibold text-dark">
@@ -156,13 +300,14 @@
             <div class="w-full px-4 md:w-1/2 xl:w-1/4">
                 <div class="mx-auto mb-10 w-full max-w-[370px]">
                     <div class="relative overflow-hidden rounded-lg">
-                        <img src="{{ asset('images/sekjen.jpeg') }}" alt="image" class="w-full h-[450px]" />
+                        <img src="{{ asset('images/sekjen.jpeg') }}" alt="image"
+                            class="object-cover w-full h-[450px]" />
                         <div class="absolute left-0 w-full text-center bottom-5">
                             <div class="relative px-3 py-5 mx-5 overflow-hidden bg-white rounded-lg">
                                 <h3 class="text-sm font-semibold text-dark">
                                     H. Zaharuddin Tambunan, Lc, S.Pd.I
                                 </h3>
-                                <p class="text-sm text-body-color">Sekjen</p>
+                                <p class="text-sm text-body-color">Sekretaris</p>
                                 <div>
                                     <span class="absolute bottom-0 left-0">
                                         <svg width="61" height="30" viewBox="0 0 61 30" fill="none"
